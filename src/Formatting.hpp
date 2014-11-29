1f5e7b128fa165d9c13c7c12ca7eade57267c1fe
Joshua Augustin <augustinspring@gmail.com>
2014-11-29 12:07:51 -0500

    Added formatting for sf:Color objects.

--- src/Formatting.hpp
+++ src/Formatting.hpp
@@ -5,9 +5,18 @@
 #include <iostream>
 #include <typeinfo>
 #include <Eigen/Dense>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 using namespace Eigen;
+
+#ifdef NOCOLOR
+  #define BOLD_GREEN(x) x
+  #define BOLD_BLUE(x)  x
+#else
+  #define BOLD_GREEN(x) "\033[1;32m" << (x) << "\033[0m"
+  #define BOLD_BLUE(x)  "\033[1;34m" << (x) << "\033[0m"
+#endif
 
 ostream& operator<<(ostream& out, const Vector3d& obj) {
   char buffer[100];
@@ -21,12 +30,11 @@ ostream& operator<<(ostream& out, const Vector2d& obj) {
   return out << buffer;
 }
 
-#ifdef NOCOLOR
-  #define BOLD_GREEN(x) x
-  #define BOLD_BLUE(x)  x
-#else
-  #define BOLD_GREEN(x) "\033[1;32m" << (x) << "\033[0m"
-  #define BOLD_BLUE(x)  "\033[1;34m" << (x) << "\033[0m"
-#endif
+ostream& operator<<(ostream& out, const sf::Color& obj) {
+    return out << BOLD_BLUE("Color")"("
+        << BOLD_GREEN("r")"=" << (int)obj.r << ", "
+        << BOLD_GREEN("g")"=" << (int)obj.g << ", "
+        << BOLD_GREEN("b")"=" << (int)obj.b << ")";
+}
 
 #endif /* end of include guard: __FORMATTING_H__ */
