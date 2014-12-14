bbd317b4ffc1fd0dcbc3f6756591733ad2b25869
Joshua Augustin <augustinspring@gmail.com>
2014-12-13 19:34:33 -0500

    Starting over with a simpler Camera.hpp.

--- src/camera/Camera.cpp
+++
@@ -1,9 +0,0 @@
-#include "Camera.hpp"
- 
-Vector3d Camera::pixel_position(size_t x, size_t y) {
-    return Vector3d(
-            _width * ((double)x - width() / 2)/width(),
-            _height * ((double)y - height() / 2)/height(),
-            _focal_length
-            );
-};
